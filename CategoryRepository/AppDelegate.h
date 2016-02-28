37a54fc584b0948508eec52d302b0dbe8d0690bc
SimleCp <vip.mobile@qq.com>
2016-02-28 16:28:56 +0800

    初始化项目

---
+++ CategoryRepository/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CategoryRepository
+//
+//  Created by pengpeng on 16/2/28.
+//  Copyright © 2016年 Swift520. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
