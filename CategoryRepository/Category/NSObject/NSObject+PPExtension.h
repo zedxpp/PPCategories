ccddcd09b90d298e9a99c032a184285337075686
SimleCp <vip.mobile@qq.com>
2016-10-31 10:27:07 +0800

    add NSObjectCategory

---
+++ CategoryRepository/Category/NSObject/NSObject+PPExtension.h
@@ -0,0 +1,15 @@
+//
+//  NSObject+PPExtension.h
+//  CategoryRepository
+//
+//  Created by pengpeng on 2016/10/31.
+//  Copyright © 2016年 Swift520. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (PPExtension)
+
+- (NSString *)logAllIvarAndValue;
+
+@end
